066667a1f400f7c50739f3333e6531b7357152c5
farud <farudbox@gmail.com>
2024-07-26 02:51:44 +0300

    KS-2-3 declaration of a new constant

--- src/main/kotlin/lesson_2/lesson2-task3.kt
+++ src/main/kotlin/lesson_2/lesson2-task3.kt
@@ -1,16 +1,18 @@
 package org.example.lesson_2
 
+const val minutesInHour = 60
+
 fun main() {
     val departureHour = 9
     val departureMinute = 39
-    val departureHourToMinutes = departureHour * 60
+    val departureHourToMinutes = departureHour * minutesInHour
     val departureTotalMinutes = departureHourToMinutes + departureMinute
 
     val travelTime = 457
 
     val arrivalTotalMinutes = departureTotalMinutes + travelTime
-    val arrivalHour = arrivalTotalMinutes / 60
-    val arrivalMinute = arrivalTotalMinutes % 60
+    val arrivalHour = arrivalTotalMinutes / minutesInHour
+    val arrivalMinute = arrivalTotalMinutes % minutesInHour
 
     println("$arrivalHour:$arrivalMinute")
 }
