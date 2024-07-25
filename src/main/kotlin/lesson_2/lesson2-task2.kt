db6a36adce9a30ac8c2e6f544ca8d00f712adc91
farud <farudbox@gmail.com>
2024-07-26 02:47:22 +0300

    KS-2-2 declaration of a new variable

--- src/main/kotlin/lesson_2/lesson2-task2.kt
+++ src/main/kotlin/lesson_2/lesson2-task2.kt
@@ -1,19 +1,21 @@
 package org.example.lesson_2
 
 fun main() {
-    val numberOfEmloyees = 50
+    val numberOfEmployees = 50
     val employeesSalaries = 30000
 
     val numberOfInterns = 30
     val internsSalaries = 20000
 
-    val employeeSalary = numberOfEmloyees * employeesSalaries
+    val totalNumberOfEmployees = numberOfInterns + numberOfEmployees
+
+    val employeeSalary = numberOfEmployees * employeesSalaries
     val internSalary = numberOfInterns * internsSalaries
     println(employeeSalary)
 
     val salaryExpenses = employeeSalary + internSalary
     println(salaryExpenses)
 
-    val averageSalary = salaryExpenses / 80
+    val averageSalary = salaryExpenses / totalNumberOfEmployees
     println(averageSalary)
 }
