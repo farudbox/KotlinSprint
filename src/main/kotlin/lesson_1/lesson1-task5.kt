56c6cf3a4d375a16aaa47aa0a5946c8bf1b0d04c
farud <farudbox@gmai.com>
2024-07-28 12:57:31 +0300

    KS-1-4 added a time constants

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,12 +1,15 @@
 package org.example.lesson_1
 
+const val UNIT_OF_TIME_IN_UNIT_OF_TIME = 60
+const val UNIT_OF_TIME_IN_UNIT_OF_TIME_ABOVE = UNIT_OF_TIME_IN_UNIT_OF_TIME * UNIT_OF_TIME_IN_UNIT_OF_TIME
+
 fun main() {
     val totalSeconds = 6480
 
-    val hours = totalSeconds / 3600
-    val remainingSeconds = totalSeconds % 3600
-    val minute = remainingSeconds / 60
-    val seconds = remainingSeconds % 60
+    val hours = totalSeconds / UNIT_OF_TIME_IN_UNIT_OF_TIME_ABOVE
+    val remainingSeconds = totalSeconds % UNIT_OF_TIME_IN_UNIT_OF_TIME_ABOVE
+    val minute = remainingSeconds / UNIT_OF_TIME_IN_UNIT_OF_TIME
+    val seconds = remainingSeconds % UNIT_OF_TIME_IN_UNIT_OF_TIME
 
     val timeStr = String.format("%02d:%02d:%02d", hours, minute, seconds) //попросил подсказку в решении у chatGPT
 
