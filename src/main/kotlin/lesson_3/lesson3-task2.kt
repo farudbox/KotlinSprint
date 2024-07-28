5a0d65da82c78c9a26dac97b5a5cbc6b46367e9e
farud <farudbox@gmai.com>
2024-07-28 20:42:11 +0300

    KS-3-2 added added data output

---
+++ src/main/kotlin/lesson_3/lesson3-task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val surname = "Сергеевна"
+    val oldLastName = "Андреева"
+    val currentLastName = "Сидорова"
+
+    val oldAge = 20
+    val currentAge = 22
+
+    println("$oldLastName $name $surname: $oldAge")
+    println("$currentLastName $name $surname: $currentAge")
+}
