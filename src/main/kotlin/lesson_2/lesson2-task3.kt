0e23b13b46a1defe38e3019db597b09009a476b2
farud <farudbox@gmai.com>
2024-07-28 13:01:28 +0300

    KS-2-3 rename const

--- src/main/kotlin/lesson_2/lesson2-task3.kt
+++ src/main/kotlin/lesson_2/lesson2-task3.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_2
 
-const val minutesInHour = 60
+const val UNIT_OF_TIME_IN_UNIT_OF_TIME = 60
 
 fun main() {
     val departureHour = 9
     val departureMinute = 39
-    val departureHourToMinutes = departureHour * minutesInHour
+    val departureHourToMinutes = departureHour * UNIT_OF_TIME_IN_UNIT_OF_TIME
     val departureTotalMinutes = departureHourToMinutes + departureMinute
 
     val travelTime = 457
 
     val arrivalTotalMinutes = departureTotalMinutes + travelTime
-    val arrivalHour = arrivalTotalMinutes / minutesInHour
-    val arrivalMinute = arrivalTotalMinutes % minutesInHour
+    val arrivalHour = arrivalTotalMinutes / UNIT_OF_TIME_IN_UNIT_OF_TIME
+    val arrivalMinute = arrivalTotalMinutes % UNIT_OF_TIME_IN_UNIT_OF_TIME
 
     println("$arrivalHour:$arrivalMinute")
 }
