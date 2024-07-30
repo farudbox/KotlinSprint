1fad11f4d7463a5b890b36ddbef14199aa8ae4ad
farud <farudbox@gmail.com>
2024-07-31 02:40:39 +0300

    KS-2-4 unnecessary comments removed

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -7,14 +7,8 @@ fun main() {
     val ironOre = 11
     val buffToMinedMaterials = 20
 
-    val buffToCrystallineOre = crystallineOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT // Делим количество добытой руды на 100% руды
-    val buffToIronOre = ironOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT // Делим количество добытой руды на 100% руды
-
-//    val formattedCrystallineOre = buffToCrystallineOre.toInt()
-//    val formattedIronOre = buffToIronOre.toInt()
-
-//    println("Количество бонусной кристаллической руды: $formattedCrystallineOre")
-//    println("Количество бонусной железной руды: $formattedIronOre")
+    val buffToCrystallineOre = crystallineOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT
+    val buffToIronOre = ironOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT
 
     println("Количество бонусной кристаллической руды: $buffToCrystallineOre")
     println("Количество бонусной железной руды: $buffToIronOre")
