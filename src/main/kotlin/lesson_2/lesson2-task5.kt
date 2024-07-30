0aa0ca5937696070e4db25b213f808f5aa7fef6b
farud <farudbox@gmail.com>
2024-07-30 12:15:14 +0300

    KS-2-5 corrected formatting

--- src/main/kotlin/lesson_2/lesson2-task5.kt
+++ src/main/kotlin/lesson_2/lesson2-task5.kt
@@ -3,27 +3,21 @@ package org.example.lesson_2
 import kotlin.math.pow
 
 /*
-
 S = x * (1 + a) ^ n
-
 S - сумма, образовавшаяся к концу срока;
-
 x - начальная сумма вложенных средств
-
 a - годовая процентная ставка
-
 n - срок вклада в годах
-
  */
 
 fun main() {
-    val initialDepositAmount    = 70_000f    // x
-    val interestRate            = 0.167      // a
-    val depositTerm             = 20         // n
+    val principal = 70_000f // x
+    val annualRate = 0.167 // a
+    val years = 20 // n
 
-    val sum = initialDepositAmount * (1 + interestRate).pow(depositTerm)
+    val sum = principal * (1 + annualRate).pow(years) // Рассчитываем будущее значение вклада по формуле сложных процентов
 
-    val formattedSum = String.format("%.3f", sum)
-
-    println("Размер вклада через 20 лет составит: $formattedSum")
+    val formattedSum = String.format("%.3f", sum) // Форматируем результат с тремя знаками после запятой
+    println("Размер вклада через $years лет составит: $formattedSum")
 }
+
