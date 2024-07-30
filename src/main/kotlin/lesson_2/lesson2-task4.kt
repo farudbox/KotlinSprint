f0b867eae39f688c32726270a8d644506398c1d0
farud <farudbox@gmail.com>
2024-07-30 11:59:13 +0300

    KS-2-5 added new constant and change buff to materials

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -1,17 +1,21 @@
 package org.example.lesson_2
 
+const val ONE_HUNDRED_PERCENT = 100
+
 fun main() {
     val crystallineOre = 7
     val ironOre = 11
+    val buffToMinedMaterials = 20
 
-    val buffToMinedMaterials = 0.2
+    val buffToCrystallineOre = crystallineOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT // Делим количество добытой руды на 100% руды
+    val buffToIronOre = ironOre * buffToMinedMaterials / ONE_HUNDRED_PERCENT // Делим количество добытой руды на 100% руды
 
-    val buffToCrystallineOre = crystallineOre * buffToMinedMaterials
-    val buffToIronOre = ironOre * buffToMinedMaterials
+//    val formattedCrystallineOre = buffToCrystallineOre.toInt()
+//    val formattedIronOre = buffToIronOre.toInt()
 
-    val formattedCrystallineOre = buffToCrystallineOre.toInt()
-    val formattedIronOre = buffToIronOre.toInt()
+//    println("Количество бонусной кристаллической руды: $formattedCrystallineOre")
+//    println("Количество бонусной железной руды: $formattedIronOre")
 
-    println("Количество бонусной кристаллической руды: $formattedCrystallineOre")
-    println("Количество бонусной железной руды: $formattedIronOre")
+    println("Количество бонусной кристаллической руды: $buffToCrystallineOre")
+    println("Количество бонусной железной руды: $buffToIronOre")
 }
