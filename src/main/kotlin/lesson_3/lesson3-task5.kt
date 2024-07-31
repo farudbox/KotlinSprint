c036548006907fc1ff1bf4909da246c47576a3f3
farud <farudbox@gmail.com>
2024-07-31 15:02:13 +0300

    KS-3-5 Improved work with functions

--- src/main/kotlin/lesson_3/lesson3-task5.kt
+++ src/main/kotlin/lesson_3/lesson3-task5.kt
@@ -1,15 +1,12 @@
 package org.example.lesson_3
 
 fun main() {
+    val moveString = "D2-D4:0"
 
-    val moveString = "D2-D4;0"
-
-    val parts = moveString.split(";")
-    val positions = parts[0].split("-")
-    val moveNumber = parts[1]
-
-    val from = positions[0]
-    val to = positions[1]
+    val parts = moveString.split("-", ":")
+    val from = parts[0]
+    val to = parts[1]
+    val moveNumber = parts[2]
 
     println("Откуда: $from")
     println("Куда: $to")
