097eab4b6427f421dba5ad38ecbd5dc66d2d35d9
farud <farudbox@gmail.com>
2024-07-31 15:07:42 +0300

    KS-4-2 added brackets

--- src/main/kotlin/lesson4/lesson4-task2.kt
+++ src/main/kotlin/lesson4/lesson4-task2.kt
@@ -10,6 +10,6 @@ fun main() {
     val averageWeightMax = 100
     val averageCapacity = 100
 
-    println("Груз с весом $cargo1Weight кг и объемом $cargo1Capacity л соответствует категории 'Average': ${(cargo1Weight > averageWeightMinimum && cargo1Weight <= averageWeightMax) and (cargo1Capacity < averageCapacity)}")
-    println("Груз с весом $cargo2Weight кг и объемом $cargo2Capacity л соответствует категории 'Average': ${(cargo2Weight > averageWeightMinimum && cargo2Weight <= averageWeightMax) and (cargo2Capacity < averageCapacity)}")
+    println("Груз с весом $cargo1Weight кг и объемом $cargo1Capacity л соответствует категории 'Average': ${((cargo1Weight > averageWeightMinimum) && (cargo1Weight <= averageWeightMax)) and (cargo1Capacity < averageCapacity)}")
+    println("Груз с весом $cargo2Weight кг и объемом $cargo2Capacity л соответствует категории 'Average': ${((cargo2Weight > averageWeightMinimum) && (cargo2Weight <= averageWeightMax)) and (cargo2Capacity < averageCapacity)}")
 }
