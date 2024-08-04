3b0cee95c5c2ddd9f2a2b0e8d355298f05441897
farud <farudbox@gmail.com>
2024-08-04 22:51:09 +0300

    KS-4-5 removed unnecessary variables

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -3,14 +3,10 @@ package org.example.lesson_4
 fun main() {
     val dayOfTraining = 5
 
-    // IDE предупреждает меня об избыточных условиях, но я не особо понимаю как их решить
-    val arms = dayOfTraining % 2 == 1
-    val legs = dayOfTraining % 2 == 0
-    val back = dayOfTraining % 2 == 0
-    val press = dayOfTraining % 2 == 1
+    val isOddDay = dayOfTraining % 2 == 1
 
-    println("Упражнения для рук: $arms")
-    println("Упражнения для ног: $legs")
-    println("Упражнения для спины: $back")
-    println("Упражнения для пресса: $press")
+    println("Упражнения для рук: $isOddDay")
+    println("Упражнения для ног: ${!isOddDay}")
+    println("Упражнения для спины: ${!isOddDay}")
+    println("Упражнения для пресса: $isOddDay")
 }
