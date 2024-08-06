0dae1ee7c81fade34d440c17db1217d8632e379a
farud <farudbox@gmail.com>
2024-08-06 17:43:59 +0300

    KS-5-1 abandoning randomness in favor of a built-in extension function

--- src/main/kotlin/lesson_5/lesson5-task1.kt
+++ src/main/kotlin/lesson_5/lesson5-task1.kt
@@ -1,10 +1,8 @@
 package org.example.lesson_5
 
-import kotlin.random.Random
-
 fun main() {
-    val num1 = Random.nextInt(1, 10)
-    val num2 = Random.nextInt(1, 10)
+    val num1 = (1..10).random()
+    val num2 = (1..10).random()
     val result = num1 + num2
 
     println("Пожалуйста, решите этот пример: $num1 + $num2")
