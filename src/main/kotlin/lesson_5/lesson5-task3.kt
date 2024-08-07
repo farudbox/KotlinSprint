a500b86deac139971f44931891aa1e0f0e9fccd0
farud <farudbox@gmail.com>
2024-08-08 00:46:38 +0300

    KS-5-3 solved bug with similarity of numbers

--- src/main/kotlin/lesson_5/lesson5-task3.kt
+++ src/main/kotlin/lesson_5/lesson5-task3.kt
@@ -4,18 +4,21 @@ fun main() {
     val num1 = (0..42).random()
     val num2 = (0..42).random()
 
+    println("$num1, $num2")
+
     println("Пожалуйста, введите первое число от 0 до 42: ")
     val userNum1 = readln().toInt()
     println("Пожалуйста, введите второе число от 0 до 42: ")
     val userNum2 = readln().toInt()
 
-    val matched1 = num1 == userNum1 || num1 == userNum2
-    val matched2 = num2 == userNum1 || num2 == userNum2
+    val randomNumbers = listOf(num1, num2)
+    val userNumbers = listOf(userNum1, userNum2)
 
-    when {
-        matched1 && matched2 -> println("Поздравляем! Вы выиграли главный приз!")
-        matched1 || matched2 -> println("Вы выиграли утешительный приз!")
-        else -> println("Неудача!")
+    val matchNumbers = randomNumbers.intersect(userNumbers.toSet()).size
+
+    when (matchNumbers) {
+        2 -> println("Поздравляем! Вы выиграли главный приз!")
+        1 -> println("Вы выиграли утешительный приз!")
     }
 
     println("Правильные числа: $num1 и $num2")
