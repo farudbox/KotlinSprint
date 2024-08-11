60cef1abd34295f5a150ac6a9e76518178157468
farud <farudbox@gmail.com>
2024-08-11 22:26:17 +0300

    KS-8-2 extra variables removed

--- src/main/kotlin/lesson_8/lesson8-task2.kt
+++ src/main/kotlin/lesson_8/lesson8-task2.kt
@@ -6,17 +6,9 @@ fun main() {
     println("Какой ингридиент вы желаете найти?")
     val userIngredient = readln()
 
-    var ingredientFound = false
-
-    for (ingredient in ingredients) {
-        if (ingredient.equals(userIngredient, ignoreCase = true)) {
-            println("Ингредиент $userIngredient в рецепте есть.")
-            ingredientFound = true
-            break
-        }
-    }
-
-    if (!ingredientFound) {
+    if (ingredients.any { it.equals(userIngredient, ignoreCase = true) }) {
+        println("Ингредиент $userIngredient в рецепте есть.")
+    } else {
         println("Такого ингредиента в рецепте нет.")
     }
 }
