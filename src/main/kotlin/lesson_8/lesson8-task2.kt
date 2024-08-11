4b5d23c69b460edff82fbc22cf502d6af84d85a3
farud <farudbox@gmail.com>
2024-08-11 22:31:23 +0300

    KS-8-2 the best solution was formed in accordance with the technical specifications

--- src/main/kotlin/lesson_8/lesson8-task2.kt
+++ src/main/kotlin/lesson_8/lesson8-task2.kt
@@ -6,9 +6,12 @@ fun main() {
     println("Какой ингридиент вы желаете найти?")
     val userIngredient = readln()
 
-    if (ingredients.any { it.equals(userIngredient, ignoreCase = true) }) {
-        println("Ингредиент $userIngredient в рецепте есть.")
-    } else {
-        println("Такого ингредиента в рецепте нет.")
+    for (ingredient in ingredients) {
+        if (ingredient.equals(userIngredient, ignoreCase = true)) {
+            println("Ингредиент $userIngredient в рецепте есть.")
+            return
+        }
     }
+
+    println("Такого ингредиента в рецепте нет.")
 }
