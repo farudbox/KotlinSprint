1e527274dd144e8b5b075be80e918f41b53e877c
farud <farudbox@gmai.com>
2024-08-14 00:18:47 +0300

    KS-10-4 complete code rewrite

--- src/main/kotlin/lesson_10/lesson10-task4.kt
+++ src/main/kotlin/lesson_10/lesson10-task4.kt
@@ -1,37 +1,57 @@
 package org.example.lesson_10
 
-fun throwTheDice(): Int {
-    return (1..6).random()
+fun main() {
+    var userWins = 0
+
+    println("Добро пожаловать в игру! Напишите \"начать\", чтобы бросить кости")
+    val agree: Boolean = readln().lowercase() == "начать"
+
+    if (agree) {
+        do {
+            val result = playRound()
+
+            if (result > 0) {
+                userWins++
+            }
+
+            println("Хотите бросить кости еще раз? Введите Да или Нет")
+            val playAgain = readln().lowercase()
+
+            if (playAgain != "да") {
+                println("Спасибо за игру! Вы выиграли $userWins партий.")
+                break
+            }
+        } while (true)
+    } else {
+        println("Игра завершена.")
+    }
 }
 
-fun playRound() {
+fun playRound(): Int {
     println("Вы сделали бросок!")
-    val userThrow = throwTheDice()
+    val userThrow: Int = throwTheDice()
     println("Вам выпало $userThrow")
 
     println("Ход противника!")
-    val compThrow = throwTheDice()
+    val compThrow: Int = throwTheDice()
     println("Противнику выпало $compThrow")
 
-    if (userThrow > compThrow) { //
-        println("Победило человечество")
-    } else if (userThrow < compThrow) {
-        println("Победила машина")
-    } else println("Ничья")
-}
-
-fun main() {
-    println("Добро пожаловать в игру! Напишите \"начать\" чтобы бросить кости")
-    val agree = readln().lowercase() == "начать"
-
-    while (agree) {
-        playRound()
-        println("Хотите бросить кости снова? (да/нет)")
-        val playAgain = readln().lowercase()
-
-        if (playAgain != "да") {
-            println("Спасибо за игру! До встречи!")
-            return
+    return when {
+        userThrow > compThrow -> {
+            println("Победило человечество")
+            1
+        }
+        userThrow < compThrow -> {
+            println("Победила машина")
+            -1
+        }
+        else -> {
+            println("Ничья!")
+            0
         }
     }
+}
+
+fun throwTheDice(): Int {
+    return (1..6).random()
 }
