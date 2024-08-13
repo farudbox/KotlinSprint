650173c3559b1937407d2247ed57b351f06a0585
farud <farudbox@gmai.com>
2024-08-13 13:26:48 +0300

    KS-9-5 minor edits + replacing the list with set

--- src/main/kotlin/lesson_9/lesson9-task5.kt
+++ src/main/kotlin/lesson_9/lesson9-task5.kt
@@ -1,21 +1,21 @@
 package org.example.lesson_9
 
 fun main() {
-    val ingredients = mutableListOf<String>()
+    val ingredients = mutableSetOf<String>()
 
     println("Введите 5 уникальных ингредиентов (по одному за раз): ")
 
     while (ingredients.size < 5) {
-        val ingredient = readln().trim().lowercase()
+        val inputIngredient = readln().trim().lowercase()
 
-        if (ingredient !in ingredients) {
-            ingredients.add(ingredient)
+        if (inputIngredient !in ingredients) {
+            ingredients.add(inputIngredient)
         } else {
-            println("Ингредиент '$ingredient' уже добавлен, введите другой: ")
+            println("Ингредиент '$inputIngredient' уже добавлен, введите другой: ")
         }
     }
 
-    ingredients.sort()
+    ingredients.sorted()
 
     val capitalizedIngredients = ingredients.mapIndexed { index, s ->
         if (index == 0) s.replaceFirstChar { it.uppercaseChar() } else s
