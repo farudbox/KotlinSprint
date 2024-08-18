de82060deb5e719f37779708c7cbbc54de0a8add
farud <farudbox@gmail.com>
2024-08-18 19:09:27 +0300

    KS-12-1 added class for storing weather information and displaying information

---
+++ src/main/kotlin/lesson_12/lesson12-task1.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_12
+
+class Weather (
+    var dayTemperature: Int,
+    var nightTemperature: Int,
+    var precipitation: Boolean,
+) {
+    fun displayWeatherInfo() {
+        println("Дневная температура: $dayTemperature")
+        println("Ночная температура: $nightTemperature")
+        println("Осадки: ${if (precipitation) "Да" else "Нет"}")
+    }
+}
+
+fun main() {
+    val weather1 = Weather(
+        dayTemperature = 25,
+        nightTemperature = 10,
+        precipitation = false,
+    )
+
+    val weather2 = Weather(
+        dayTemperature = 15,
+        nightTemperature = 5,
+        precipitation = true,
+    )
+
+    println(weather1.displayWeatherInfo())
+    println()
+    println(weather2.displayWeatherInfo())
+}
