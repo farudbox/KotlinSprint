6dd21e8baaf4d368936c7988707aec0077f6af09
farud <farudbox@gmail.com>
2024-08-21 16:12:46 +0300

    KS-14-1 changes have been made in accordance with the comments

--- src/main/kotlin/lesson_14/lesson14-task1.kt
+++ src/main/kotlin/lesson_14/lesson14-task1.kt
@@ -1,42 +1,46 @@
 package org.example.lesson_14
 
 open class Liner(
+    val name: String,
     open val speed: Int = 30,
     open val capacity: Int = 3000,
+    open val cargoCapacity: Int = 0,
 ) {
     open fun description() {
-        println("Грузовой корабль со скоростью $speed узлов и вместимостью в $capacity пассажиров.")
+        println("Лайнер $name со скоростью $speed узлов и вместимостью в $capacity пассажиров.")
     }
 }
 
 class CargoShip (
-    override val speed: Int = 20,
-    private val cargoCapacity: Int = 2000,
-) : Liner(speed = speed, capacity = 0) {
+    name: String,
+    speed: Int = 20,
+    cargoCapacity: Int = 2000,
+) : Liner(name, speed, cargoCapacity) {
 
     override fun description() {
-        println("Это грузовой корабль со скоростью $speed узлов и грузоподъемностью $cargoCapacity тонн.")
+        println("Это грузовой корабль $name со скоростью $speed узлов и грузоподъемностью $cargoCapacity тонн.")
     }
 }
 
 class Icebreaker (
-    override val speed: Int = 10,
-    override val capacity: Int = 1000,
-) : Liner(speed = speed, capacity = capacity) {
+    name: String,
+    speed: Int = 10,
+    capacity: Int = 1000,
+) : Liner(name, speed, capacity) {
 
     fun breakIce() {
         println("Ледокол колет лед на своем пути.")
     }
 
     override fun description() {
-        println("Это ледокол со скоростью $speed узлов и вместимостью $capacity пассажиров.")
+        println("Это ледокол $name со скоростью $speed узлов и вместимостью $capacity пассажиров.")
     }
 }
 
 fun main() {
-    val liner = Liner()
-    val cargoShip = CargoShip()
-    val icebreaker = Icebreaker()
+    val liner = Liner("Titanic")
+    val cargoShip = CargoShip("Alice")
+    val icebreaker = Icebreaker("North")
 
     liner.description()
     cargoShip.description()
