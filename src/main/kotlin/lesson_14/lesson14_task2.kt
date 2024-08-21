b4f9878a9be5fcb8ca0002864050fa5dd6badca5
farud <farudbox@gmail.com>
2024-08-21 16:28:31 +0300

    KS-14-2 changes have been made in accordance with the existing comments

--- src/main/kotlin/lesson_14/lesson14_task2.kt
+++ src/main/kotlin/lesson_14/lesson14_task2.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_14
 
-open class Liner1(
+open class Liner(
+    val name: String,
     open val speed: Int = 30,
     open val capacity: Int = 3000,
+    open val cargoCapacity: Int = 0,
 ) {
     open fun description() {
-        println("Это лайнер со скоростью $speed узлов и вместимостью в $capacity пассажиров.")
+        println("Лайнер $name со скоростью $speed узлов и вместимостью в $capacity пассажиров.")
     }
 
     open fun loadingMethod() {
@@ -13,13 +15,15 @@ open class Liner1(
     }
 }
 
-class CargoShip1 (
-    override val speed: Int = 20,
-    private val cargoCapacity: Int = 2000,
-) : Liner1(speed = speed, capacity = 0) {
+class CargoShip(
+    name: String,
+    speed: Int = 20,
+    capacity: Int = 0,
+    cargoCapacity: Int = 2000,
+) : Liner(name, speed, capacity, cargoCapacity) {
 
     override fun description() {
-        println("Это грузовой корабль со скоростью $speed узлов и грузоподъемностью $cargoCapacity тонн.")
+        println("Это грузовой корабль $name со скоростью $speed узлов и грузоподъемностью $cargoCapacity тонн.")
     }
 
     override fun loadingMethod() {
@@ -27,17 +31,23 @@ class CargoShip1 (
     }
 }
 
-class Icebreaker1 (
-    override val speed: Int = 10,
-    override val capacity: Int = 1000,
-) : Liner1(speed = speed, capacity = capacity) {
+class Icebreaker(
+    name: String,
+    speed: Int = 10,
+    capacity: Int = 1000,
+    cargoCapacity: Int = 1000,
+) : Liner(name, speed, capacity, cargoCapacity) {
 
     fun breakIce() {
         println("Ледокол колет лед на своем пути.")
     }
 
     override fun description() {
-        println("Это ледокол со скоростью $speed узлов и вместимостью $capacity пассажиров.")
+        println(
+            "Это ледокол $name со скоростью $speed узлов," +
+                    " вместимостью $capacity пассажиров" +
+                    " и грузоподъемностью $cargoCapacity тонн."
+        )
     }
 
     override fun loadingMethod() {
@@ -46,9 +56,9 @@ class Icebreaker1 (
 }
 
 fun main() {
-    val liner = Liner1()
-    val cargoShip = CargoShip1()
-    val icebreaker = Icebreaker1()
+val liner = Liner("Titanic")
+val cargoShip = CargoShip("Alice")
+val icebreaker = Icebreaker("North")
 
     liner.description()
     liner.loadingMethod()
