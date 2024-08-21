3b679aeb94cec3511f457e79c80aa86193d7608b
farud <farudbox@gmail.com>
2024-08-21 16:32:37 +0300

    KS-14-3 colors were moved to constants

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -3,12 +3,15 @@ package org.example.lesson_14
 import kotlin.math.PI
 import kotlin.math.pow
 
-abstract class Figure (val color: String) {
+const val COLOR_BLACK = "black"
+const val COLOR_WHITE = "white"
+
+abstract class Figure(val color: String) {
     abstract fun area(): Double
     abstract fun perimeter(): Double
 }
 
-class Circle (color: String, val radius: Double): Figure(color) {
+class Circle(color: String, val radius: Double): Figure(color) {
     override fun area(): Double {
         return PI * radius.pow(2)
     }
@@ -18,7 +21,7 @@ class Circle (color: String, val radius: Double): Figure(color) {
     }
 }
 
-class Rectangle(color: String, val width: Double, val height: Double) : Figure(color) {
+class Rectangle(color: String, val width: Double, val height: Double): Figure(color) {
     override fun area(): Double {
         return width * height
     }
@@ -30,20 +33,20 @@ class Rectangle(color: String, val width: Double, val height: Double) : Figure(c
 
 fun main() {
     val figures = listOf(
-        Circle("black", 5.0),
-        Circle("white", 3.0),
-        Rectangle("black", 4.0, 7.0),
-        Rectangle("white", 6.0, 8.0),
-        Circle("black", 2.5),
-        Rectangle("white", 3.0, 4.0)
+        Circle(color = COLOR_BLACK, radius = 5.0),
+        Circle(color = COLOR_WHITE, radius = 3.0),
+        Rectangle(color = COLOR_BLACK, width = 4.0, height = 7.0),
+        Rectangle(color = COLOR_WHITE, width = 6.0, height = 8.0),
+        Circle(color = COLOR_BLACK, radius = 2.5),
+        Rectangle(color = COLOR_WHITE, width = 3.0, height = 4.0)
     )
 
     val blackPerimetersSum = figures
-        .filter { it.color == "black" }
+        .filter { it.color == COLOR_BLACK }
         .sumOf { it.perimeter() }
 
     val whiteAreasSum = figures
-        .filter { it.color == "white" }
+        .filter { it.color == COLOR_WHITE }
         .sumOf { it.area() }
 
     println("Сумма периметров всех черных фигур: $blackPerimetersSum")
