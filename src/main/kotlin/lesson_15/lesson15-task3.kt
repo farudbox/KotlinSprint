0c2e49b3c86886104662141ac36569f5ed03aee3
farud <farudbox@gmail.com>
2024-08-21 17:21:53 +0300

    KS-15-3 fun deleteUser & deleteMessage transfer to Admin

--- src/main/kotlin/lesson_15/lesson15-task3.kt
+++ src/main/kotlin/lesson_15/lesson15-task3.kt
@@ -10,28 +10,16 @@ abstract class Everyone(
     open fun sendMessage(message: String) {
         println("$username отправил сообщение \"$message\"")
     }
-
-    abstract fun deleteMessage(messageId: Int)
-
-    abstract fun deleteUser(user: Everyone)
 }
 
-class User (username: String) : Everyone(username) {
-    override fun deleteUser(user: Everyone) {
-        println("$username не имеет права на удаление пользователя")
-    }
-
-    override fun deleteMessage(messageId: Int) {
-        println("$username не имеет права на удаление сообщений")
-    }
-}
+class User (username: String) : Everyone(username)
 
 class Admin (username: String) : Everyone(username) {
-    override fun deleteUser(user: Everyone) {
+    fun deleteUser(user: Everyone) {
         println("Пользователь $user был удален")
     }
 
-    override fun deleteMessage(messageId: Int) {
+    fun deleteMessage(messageId: Int) {
         println("Сообщение $messageId было удалено")
     }
 }
@@ -45,9 +33,6 @@ fun main() {
 
     user2.sendMessage("Привет")
     user1.readMessage()
-
-    user1.deleteMessage(123)
-    user1.deleteUser(user2)
 
     user2.deleteMessage(123)
     user2.deleteUser(user1)
