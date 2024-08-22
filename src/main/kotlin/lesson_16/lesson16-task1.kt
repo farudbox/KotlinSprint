43d509fc524fab2f5e634d2e26a910376bea3227
farud <farudbox@gmai.com>
2024-08-22 13:37:37 +0300

    KS-16-1 rename the fun name

--- src/main/kotlin/lesson_16/lesson16-task1.kt
+++ src/main/kotlin/lesson_16/lesson16-task1.kt
@@ -3,13 +3,13 @@ package org.example.lesson_16
 class Dice {
     private val fieldNum: Int = (0..6).random()
 
-    fun fieldView() {
+    fun showDiceField() {
         println("Выпавшее число: $fieldNum")
     }
 }
 
 fun main() {
     val dice = Dice()
-    dice.fieldView()
+    dice.showDiceField()
 }
 
