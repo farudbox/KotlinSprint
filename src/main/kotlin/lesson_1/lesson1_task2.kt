789121ebd1f1b315e6028365ed03a8f367b78de5
farud <farudbox@gmai.com>
2024-08-22 22:53:48 +0300

    master update

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders = 75
+    val textGratitude = "Благодарим вас за покупку! Возвращайтесь ещё!"
+    var numberOfEmployees = 2000
+
+    println(numberOfOrders)
+    println(textGratitude)
+
+    numberOfEmployees -= 1
+    println(numberOfEmployees)
+}
