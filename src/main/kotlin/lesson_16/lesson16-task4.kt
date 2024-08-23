02dca036f55a9a305daab37b770c16ce9d55a9b7
farud <farudbox@gmail.com>
2024-08-23 23:24:27 +0300

    KS-16-4 added new fun requestOrderStatusChange

--- src/main/kotlin/lesson_16/lesson16-task4.kt
+++ src/main/kotlin/lesson_16/lesson16-task4.kt
@@ -2,15 +2,19 @@ package org.example.lesson_16
 
 class Order(private val orderNumber: Int, private var orderStatus: String) {
 
-    fun changeOrderStatus(newStatus: String) {
-        println("Запрос передан менеджеру")
+    private fun changeOrderStatus(newStatus: String) {
         orderStatus = newStatus
         println("Статус заказа $orderNumber изменен на $orderStatus")
+    }
+
+    fun requestOrderStatusChange(newStatus: String) {
+        println("Запрос передан менеджеру")
+        changeOrderStatus(newStatus)
     }
 }
 
 fun main() {
     val order = Order(15, "В пути")
 
-    order.changeOrderStatus("Готов к выдаче")
+    order.requestOrderStatusChange("Готов к выдаче")
 }
