5e94af6a7a8314999f5d9fd8ef402d10c31e9382
farud <farudbox@gmai.com>
2024-08-26 23:07:27 +0300

    KS-18-2 the code has been corrected in accordance with the existing comments

--- src/main/kotlin/lesson_18/lesson18-task2.kt
+++ src/main/kotlin/lesson_18/lesson18-task2.kt
@@ -1,36 +1,35 @@
 package org.example.lesson_18
 
 abstract class Dice (val sides: Int) {
-    abstract fun roll()
-}
-
-class D4 : Dice(4) {
-    override fun roll() {
-        val result = (1..sides).random()
-        println("D4 rolled: $result")
+    open fun roll() {
+        println("Roll: ${(1..sides).random()}")
     }
 }
 
-class D6 : Dice(6) {
+class TetrahedralDice : Dice(4) {
     override fun roll() {
-        val result = (1..sides).random()
-        println("D6 rolled: $result")
+        println("D4 roll: ${(1..sides).random()}")
     }
 }
 
-class D8 : Dice(8) {
+class HexagonalDice : Dice(6) {
     override fun roll() {
-        val result = (1..sides).random()
-        println("D8 rolled: $result")
+        println("D6 roll: ${(1..sides).random()}")
+    }
+}
+
+class OctagonalDice : Dice(8) {
+    override fun roll() {
+        println("D8 roll: ${(1..sides).random()}")
     }
 }
 
 fun main() {
-    val dice4 = D4()
-    val dice6 = D6()
-    val dice8 = D8()
+    val dice4 = TetrahedralDice()
+    val dice6 = HexagonalDice()
+    val dice8 = OctagonalDice()
 
-    val diceList = listOf(dice4, dice6, dice8)
+    val diceList: List<Dice> = listOf(dice4, dice6, dice8)
 
     for (dice in diceList) dice.roll()
 }
