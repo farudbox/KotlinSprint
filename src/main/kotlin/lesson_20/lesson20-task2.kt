57e5140f270c67a4168d5445e847156a1aba13a5
farud <farudbox@gmail.com>
2024-08-31 22:14:55 +0300

    KS-20-2 minor code improvements

--- src/main/kotlin/lesson_20/lesson20-task2.kt
+++ src/main/kotlin/lesson_20/lesson20-task2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_20
 
-data class Player(val name: String, var hp: Int, val maxHp: Int)
+data class PlayerHeal(val name: String, var hp: Int, val maxHp: Int)
 
 fun main() {
-    val healPotion: (Player) -> Unit = { player: Player ->
+    val healPotion: (PlayerHeal) -> Unit = { player: PlayerHeal ->
         player.hp = player.maxHp
         println("Пользователь ${player.name} восстановил здоровье. " +
                 "Текущее здоровье: ${player.hp}")
     }
 
-    val player = Player("Артас", 15, 20)
+    val player = PlayerHeal("Артас", 15, 20)
 
     healPotion(player)
 }
