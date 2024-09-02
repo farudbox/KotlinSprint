aaa51cb73867af7a588700cf37a5646f3c5a6c45
farud <farudbox@gmail.com>
2024-09-02 20:24:39 +0300

    KS-20-5 minor code improvements

--- src/main/kotlin/lesson_20/lesson20-task5.kt
+++ src/main/kotlin/lesson_20/lesson20-task5.kt
@@ -1,7 +1,5 @@
 package org.example.lesson_20
 
-import kotlin.random.Random
-
 class Robot {
     private val phrases = listOf(
         "Привет, мир!",
@@ -11,16 +9,14 @@ class Robot {
         "Что нового в мире технологий?",
     )
 
-    private var modifier: ((String) -> String)? = null
+    private var modifier: (String) -> String = { it }
 
     fun setModifier(modifier: (String) -> String) {
         this.modifier = modifier
     }
 
     fun say() {
-        val phrase = phrases[Random.nextInt(phrases.size)]
-        val modifiedPhrase = modifier?.invoke(phrase) ?: phrase
-        println(modifiedPhrase)
+        println(modifier(phrases.random()))
     }
 }
 
